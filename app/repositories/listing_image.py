fd6336811cc9c79b65cae5d30b62fdd563c3ac79
AlexSvet117 <alex.svet117@gmail.com>
2025-05-18 20:10:26 +0100

    more repos

---
+++ app/repositories/listing_image.py
@@ -0,0 +1,11 @@
+from typing import Optional, List
+from app.models.listing import Listing
+from app.extensions import db
+from sqlalchemy import select
+
+class ListingImageRepository:
+
+    @staticmethod
+    def create():
+        
+        pass
---
+++ app/repositories/listing_repository.py
@@ -0,0 +1,40 @@
+from typing import Optional, List
+from app.models.listing import Listing
+from app.extensions import db
+from sqlalchemy import select
+
+class ListingRepository:
+
+    @staticmethod
+    def create(owner_id: int, make: str, model: str, body_type: enumerate, **kwargs) -> Listing :
+        listing = Listing(owner_id=owner_id, make=make, model=model, body_type=body_type)
+        #set additional attributes
+        for key, value in kwargs.items():
+            if hasattr(listing, key):
+                setattr(listing, key, value)
+        db.session.add(listing)
+        db.session.commit()
+        return listing
+    
+    @staticmethod
+    def get_by_id(listing_id: int) -> Optional[Listing]:
+        stmt = select(Listing).where(Listing.id == listing_id)
+        result = db.session.execute(stmt)
+        return result.scalars().first()
+    
+    @staticmethod
+    def get_by_owner_id(owner_id: int) -> List[Listing]:
+        stmt = select(Listing).where(Listing.owner_id == owner_id)
+        results = db.session.execute(stmt)
+        return list(results.scalars().all())
+    
+    @staticmethod
+    def update(listing: Listing) -> Listing:
+        db.session.commit()
+        return listing
+    
+    @staticmethod
+    def delete(listing: Listing) -> bool:
+        db.session.delete(listing)
+        db.session.commit()
+        return True
